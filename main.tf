7eae70a73aabdf71e7d3f368e6ed434e580c1681
imjoseangel <josea.munoz@gmail.com>
2021-06-02 07:55:29 +0200

    add storage account

---
+++ main.tf
@@ -0,0 +1,178 @@
+#-------------------------------
+# Local Declarations
+#-------------------------------
+locals {
+  account_tier             = (var.account_kind == "FileStorage" ? "Premium" : split("_", var.skuname)[0])
+  account_replication_type = (local.account_tier == "Premium" ? "LRS" : split("_", var.skuname)[1])
+
+  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
+  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
+}
+
+#---------------------------------------------------------
+# Resource Group Creation or selection - Default is "false"
+#----------------------------------------------------------
+data "azurerm_resource_group" "rgrp" {
+  count = var.create_resource_group == false ? 1 : 0
+  name  = var.resource_group_name
+}
+
+resource "azurerm_resource_group" "rg" {
+  count    = var.create_resource_group ? 1 : 0
+  name     = lower(var.resource_group_name)
+  location = var.location
+  tags     = merge({ "ResourceName" = format("%s", var.resource_group_name) }, var.tags, )
+}
+
+#---------------------------------------------------------
+# Storage Account Creation or selection
+#----------------------------------------------------------
+resource "random_string" "unique" {
+  length  = 10
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "storeacc" {
+  name                      = format("st%s%s", lower(replace(var.storage_account_name, "/[[:^alnum:]]/", "")), random_string.unique.result)
+  resource_group_name       = local.resource_group_name
+  location                  = local.location
+  account_kind              = var.account_kind
+  account_tier              = local.account_tier
+  account_replication_type  = local.account_replication_type
+  enable_https_traffic_only = true
+  allow_blob_public_access  = var.enable_advanced_threat_protection == true ? true : false
+  tags                      = merge({ "ResourceName" = format("sta%s%s", lower(replace(var.storage_account_name, "/[[:^alnum:]]/", "")), random_string.unique.result) }, var.tags, )
+
+  blob_properties {
+    versioning_enabled = true
+    delete_retention_policy {
+      days = var.soft_delete_retention
+    }
+  }
+
+  dynamic "network_rules" {
+    for_each = var.network_rules != null ? ["true"] : []
+    content {
+      default_action             = "Deny"
+      bypass                     = var.network_rules.bypass
+      ip_rules                   = var.network_rules.ip_rules
+      virtual_network_subnet_ids = var.network_rules.subnet_ids
+    }
+  }
+}
+
+# resource "azurerm_template_deployment" "storeacc" {
+#   name                = azurerm_storage_account.storeacc.name
+#   resource_group_name = local.resource_group_name
+#   deployment_mode     = "Incremental"
+#   parameters = {
+#     "storageAccount" = azurerm_storage_account.storeacc.name
+#   }
+
+#   template_body = <<DEPLOY
+#         {
+#             "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+#             "contentVersion": "1.0.0.0",
+#             "parameters": {
+#                 "storageAccount": {
+#                     "type": "string",
+#                     "metadata": {
+#                         "description": "Storage Account Name"}
+#                 }
+#             },
+#             "variables": {},
+#             "resources": [
+#                 {
+#                     "type": "Microsoft.Storage/storageAccounts/blobServices",
+#                     "apiVersion": "2019-06-01",
+#                     "name": "[concat(parameters('storageAccount'), '/default')]",
+#                     "properties": {
+#                         "IsVersioningEnabled": true
+#                     }
+#                 }
+#             ]
+#         }
+#     DEPLOY
+
+#   depends_on = [
+#     azurerm_storage_account.storeacc,
+#   ]
+# }
+
+#--------------------------------------
+# Storage Advanced Threat Protection 
+#--------------------------------------
+resource "azurerm_advanced_threat_protection" "atp" {
+  target_resource_id = azurerm_storage_account.storeacc.id
+  enabled            = var.enable_advanced_threat_protection
+}
+
+#-------------------------------
+# Storage Container Creation
+#-------------------------------
+resource "azurerm_storage_container" "container" {
+  count                 = length(var.containers_list)
+  name                  = var.containers_list[count.index].name
+  storage_account_name  = azurerm_storage_account.storeacc.name
+  container_access_type = var.containers_list[count.index].access_type
+}
+
+#-------------------------------
+# Storage Fileshare Creation
+#-------------------------------
+resource "azurerm_storage_share" "fileshare" {
+  count                = length(var.file_shares)
+  name                 = var.file_shares[count.index].name
+  storage_account_name = azurerm_storage_account.storeacc.name
+  quota                = var.file_shares[count.index].quota
+}
+
+#-------------------------------
+# Storage Tables Creation
+#-------------------------------
+resource "azurerm_storage_table" "tables" {
+  count                = length(var.tables)
+  name                 = var.tables[count.index]
+  storage_account_name = azurerm_storage_account.storeacc.name
+}
+
+#-------------------------------
+# Storage Queue Creation
+#-------------------------------
+resource "azurerm_storage_queue" "queues" {
+  count                = length(var.queues)
+  name                 = var.queues[count.index]
+  storage_account_name = azurerm_storage_account.storeacc.name
+}
+
+#-------------------------------
+# Storage Lifecycle Management
+#-------------------------------
+resource "azurerm_storage_management_policy" "lcpolicy" {
+  count              = length(var.lifecycles) == 0 ? 0 : 1
+  storage_account_id = azurerm_storage_account.storeacc.id
+
+  dynamic "rule" {
+    for_each = var.lifecycles
+    iterator = rule
+    content {
+      name    = "rule${rule.key}"
+      enabled = true
+      filters {
+        prefix_match = rule.value.prefix_match
+        blob_types   = ["blockBlob"]
+      }
+      actions {
+        base_blob {
+          tier_to_cool_after_days_since_modification_greater_than    = rule.value.tier_to_cool_after_days
+          tier_to_archive_after_days_since_modification_greater_than = rule.value.tier_to_archive_after_days
+          delete_after_days_since_modification_greater_than          = rule.value.delete_after_days
+        }
+        snapshot {
+          delete_after_days_since_creation_greater_than = rule.value.snapshot_delete_after_days
+        }
+      }
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,84 @@
+variable "create_resource_group" {
+  description = "Whether to create resource group and use it for all networking resources"
+  default     = false
+}
+
+variable "resource_group_name" {
+  description = "A container that holds related resources for an Azure solution"
+  default     = "rg-demo-westeurope-01"
+}
+
+variable "location" {
+  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
+  default     = "westeurope"
+}
+
+variable "storage_account_name" {
+  description = "The name of the azure storage account"
+  default     = ""
+}
+
+variable "account_kind" {
+  description = "The type of storage account. Valid options are BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2."
+  default     = "StorageV2"
+}
+
+variable "skuname" {
+  description = "The SKUs supported by Microsoft Azure Storage. Valid options are Premium_LRS, Premium_ZRS, Standard_GRS, Standard_GZRS, Standard_LRS, Standard_RAGRS, Standard_RAGZRS, Standard_ZRS"
+  default     = "Standard_RAGRS"
+}
+
+variable "access_tier" {
+  description = "Defines the access tier for BlobStorage and StorageV2 accounts. Valid options are Hot and Cool."
+  default     = "Hot"
+}
+variable "soft_delete_retention" {
+  description = "Number of retention days for soft delete. If set to null it will disable soft delete all together."
+  default     = 30
+}
+
+variable "enable_advanced_threat_protection" {
+  description = "Boolean flag which controls if advanced threat protection is enabled."
+  default     = false
+}
+
+variable "network_rules" {
+  description = "Network rules restricing access to the storage account."
+  type        = object({ bypass = list(string), ip_rules = list(string), subnet_ids = list(string) })
+  default     = null
+}
+
+variable "containers_list" {
+  description = "List of containers to create and their access levels."
+  type        = list(object({ name = string, access_type = string }))
+  default     = []
+}
+
+variable "file_shares" {
+  description = "List of containers to create and their access levels."
+  type        = list(object({ name = string, quota = number }))
+  default     = []
+}
+
+variable "queues" {
+  description = "List of storages queues"
+  type        = list(string)
+  default     = []
+}
+
+variable "tables" {
+  description = "List of storage tables."
+  type        = list(string)
+  default     = []
+}
+variable "lifecycles" {
+  description = "Configure Azure Storage firewalls and virtual networks"
+  type        = list(object({ prefix_match = set(string), tier_to_cool_after_days = number, tier_to_archive_after_days = number, delete_after_days = number, snapshot_delete_after_days = number }))
+  default     = []
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
