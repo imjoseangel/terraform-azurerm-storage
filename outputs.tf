7e503b51fff62cc7fe0834a73d53db40e1a99217
imjoseangel <josea.munoz@gmail.com>
2021-12-13 22:10:22 +0100

    fix outputs

--- outputs.tf
+++ outputs.tf
@@ -15,44 +15,44 @@ output "resource_group_location" {
 
 output "storage_account_id" {
   description = "The ID of the storage account."
-  value       = azurerm_storage_account.storeacc.id
+  value       = azurerm_storage_account.main.id
 }
 
 output "storage_account_name" {
   description = "The name of the storage account."
-  value       = azurerm_storage_account.storeacc.name
+  value       = azurerm_storage_account.main.name
 }
 
 output "storage_account_primary_location" {
   description = "The primary location of the storage account"
-  value       = azurerm_storage_account.storeacc.primary_location
+  value       = azurerm_storage_account.main.primary_location
 }
 
 output "storage_account_primary_web_endpoint" {
   description = "The endpoint URL for web storage in the primary location."
-  value       = azurerm_storage_account.storeacc.primary_web_endpoint
+  value       = azurerm_storage_account.main.primary_web_endpoint
 }
 
 output "storage_account_primary_web_host" {
   description = "The hostname with port if applicable for web storage in the primary location."
-  value       = azurerm_storage_account.storeacc.primary_web_host
+  value       = azurerm_storage_account.main.primary_web_host
 }
 
 output "storage_primary_connection_string" {
   description = "The primary connection string for the storage account."
-  value       = azurerm_storage_account.storeacc.primary_connection_string
+  value       = azurerm_storage_account.main.primary_connection_string
   sensitive   = true
 }
 
 output "storage_primary_access_key" {
   description = "The primary access key for the storage account."
-  value       = azurerm_storage_account.storeacc.primary_access_key
+  value       = azurerm_storage_account.main.primary_access_key
   sensitive   = true
 }
 
 output "storage_secondary_access_key" {
   description = "The primary access key for the storage account."
-  value       = azurerm_storage_account.storeacc.secondary_access_key
+  value       = azurerm_storage_account.main.secondary_access_key
   sensitive   = true
 }
 
